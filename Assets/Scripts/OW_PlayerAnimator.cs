255c253b67265545f4e05c4ae649d7b6459bdce2
Blakely Mayhall <blakely.c.mayhall@gmail.com>
2024-01-21 22:11:50 -0600

    refactored to use a sequencer, also fixed a minor issue with animation

--- Assets/Scripts/OW_PlayerAnimator.cs
+++ Assets/Scripts/OW_PlayerAnimator.cs
@@ -11,5 +11,41 @@ public class OW_PlayerAnimator : OW_Animator
 
     /* PRIVATE VARS */
     //*************************************************************************
+    private OW_Player player;
     //*************************************************************************
+
+    protected override void Start()
+    {
+        base.Start();
+
+        walkTime = 0.25f;
+        runTime = 0.15f;
+        player = GetComponent<OW_Player>();
+    }
+
+    protected override void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (player.playerMode != OW_PlayerModes.MOVING)
+        {
+            timer = 0f;
+            spriteIndex = stillSprite;
+            spriteRenderer.sprite = directionSprites[spriteIndex];
+            return;
+        }
+
+        if (timer > (mechanics.isSprinting ? runTime : walkTime))
+        {
+            timer = 0f;
+            spriteIndex = (spriteIndex + 1) % directionSprites.Count;
+            spriteRenderer.sprite = directionSprites[spriteIndex];
+        }
+    }
+
+    public void ShowWalkingSprite()
+    {
+        spriteIndex = 1;
+        spriteRenderer.sprite = directionSprites[spriteIndex];
+    }
 }
